1ca0e99f97b406ca4b1305609ab944ecd22e4fd2
SamBit <mohan_gi@hotmail.com>
2026-04-15 10:19:14 +0300

    fix csv encoding for superstore dataset

--- src/data_prep/load_data.py
+++ src/data_prep/load_data.py
@@ -3,5 +3,5 @@ import pandas as pd
 
 def load_superstore(path: str) -> pd.DataFrame:
     """Load the Superstore CSV dataset from the given file path."""
-    df = pd.read_csv(path)
+    df = pd.read_csv(path, encoding="windows-1252")
     return df
